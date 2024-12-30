2aaf2d43e4beee425e86be79b3bd94d62e89eae6
崔嘉琪 <sdad3777@gmail.com>
2024-12-30 11:38:35 +0800

    0.0.2
    之前一直多打了一个.0实在是给我自己都气笑了
    给开关美化了一下，然后加了点小动画

--- scripts/popup.ts
+++ scripts/popup.ts
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const switchElement = document.getElementById('switch') as HTMLInputElement;
+    const switchLabel = switchElement.parentElement as HTMLLabelElement;
 
     switchElement.checked = (await chrome.storage.sync.get(['featureEnabled']) as { featureEnabled: boolean } ?? false).featureEnabled;
+    // 打开开关动画
+    // 大概是因为在这里设置会抢在绘制任务之前把类移除吧，所以这里设置了一个小延时，等绘制任务完成后再移除
+    // 应该有更优雅的做法，但是现在就先这样，又不是不能用，说到底本来咱就不是专业的前端
+    setTimeout(() => switchLabel.classList.remove('no-transition'), 10);
 
     switchElement.addEventListener('change', () => {
         const isEnabled = switchElement.checked;
